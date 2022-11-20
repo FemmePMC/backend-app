3743d58ee38f2bd0f4218bc0a3d785dd2c7a14f5
Daniel Aguilera <d.aguilera@uniandes.edu.co>
2022-11-20 17:34:04 -0500

    Update urls.py

--- femme_back/location/urls.py
+++ femme_back/location/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.getRoute),
-    path('nombre/ubicacion/create/', views.createUbicacion),
-    path('nombre/ubicacion/<str:pk>/delete', views.deleteUbicacion),
-    path('nombre/ubicacion/<str:pk>/', views.getUbicacion),
+    path('user/location/create/', views.createUbicacion),
+    path('user/location/<str:pk>/delete', views.deleteUbicacion),
+    path('user/location/<str:pk>/', views.getUbicacion),
 ]
