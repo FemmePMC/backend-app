8d7da4464ff6075e8099eb13e2df9b678f5c3a5c
Daniel Aguilera <d.aguilera@uniandes.edu.co>
2022-11-20 17:16:55 -0500

    =

--- femme_back/forum/models.py
+++ femme_back/forum/models.py
@@ -4,6 +4,8 @@ from user.models import User
 # Create your models here.
 
 class Forum(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
     nombre = models.CharField(max_length=50)
     tema = models.CharField(max_length=50)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner')
--- femme_back/user/urls.py
+++ femme_back/user/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('',views.getRoute),
-    path('nombres/', views.getUsers),
-    path('nombre/create/', views.createUser),
-    path('nombre/<str:pk>/update', views.updateUser),
-    path('nombre/<str:pk>/delete', views.deleteUser),
-    path('nombre/<str:pk>/', views.getUser),
+    path('users/', views.getUsers),
+    path('user/create/', views.createUser),
+    path('user/<str:pk>/update', views.updateUser),
+    path('user/<str:pk>/delete', views.deleteUser),
+    path('user/<str:pk>/', views.getUser),
     
 ]
