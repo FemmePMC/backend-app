2ece6e4d0accfb55fa4d1a3a5ea57d6f05a18f32
Daniel Aguilera <d.aguilera@uniandes.edu.co>
2022-11-20 21:07:44 -0500

    Update 0002_auto_20221116_1820.py

--- femme_back/forum/migrations/0002_auto_20221116_1820.py
+++ femme_back/forum/migrations/0002_auto_20221116_1820.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='forum',
             name='owner',
-            field=models.ForeignKey(default=django.utils.timezone.now, on_delete=django.db.models.deletion.CASCADE, related_name='owner', to='user.user'),
+            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, related_name='owner', to='user.user'),
             preserve_default=False,
         ),
         migrations.AlterField(
