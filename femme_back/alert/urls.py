0f778e54f7b222ecf152846d94d86fac1efd07f4
Daniel Aguilera <d.aguilera@uniandes.edu.co>
2022-11-21 19:04:54 -0500

    Update urls.py

--- femme_back/alert/urls.py
+++ femme_back/alert/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.getRoute),
-    path('alert/<str:pk>/', views.getAlert),
     path('alert/create/', views.createAlert),
+    path('alert/<str:pk>/', views.getAlert),
     path('alert/<str:pk>/delete', views.deleteAlert),
 ]
