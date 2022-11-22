b9c54c402c2660428407c348e6714197de03dbde
Daniel Aguilera <d.aguilera@uniandes.edu.co>
2022-11-21 19:16:17 -0500

    Update models.py

--- femme_back/alert/models.py
+++ femme_back/alert/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from user.models import User
 
 class Alert(models.Model):
     message = models.CharField(max_length=200)
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='alert_user')
     messages = models.ManyToManyField('self', related_name='messages')
